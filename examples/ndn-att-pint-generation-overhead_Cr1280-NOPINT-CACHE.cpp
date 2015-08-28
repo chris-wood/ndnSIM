fe43adccf495a81fbd56748fb8b30697814861b6
Cesar Ghali <cghali@uci.edu>
2015-08-27 21:29:59 -0700

    Get rid of extra std::cout statements

--- examples/ndn-att-pint-generation-overhead_Cr1280-NOPINT-CACHE.cpp
+++ examples/ndn-att-pint-generation-overhead_Cr1280-NOPINT-CACHE.cpp
@@ -41,7 +41,6 @@ namespace ns3 {
   void
   ForwardingDelay(ns3::Time eventTime, float delay)
   {
-    cout << "man..."<<  endl;
     delayFile << eventTime.GetNanoSeconds() << "\t" << delay * 1000000000 << "\n";
   }
 
